b058956101e36e44fc5bf5cb05da45d7756db932
pbaezr <43101582+pbaezr@users.noreply.github.com>
2018-09-08 21:51:16 -0300

    Update getclipdata.m

--- getclipdata.m
+++ getclipdata.m
@@ -4,8 +4,21 @@ function data = getclipdata()
 %
 %   data = getclipdata
 
-% Author: Pablo Báez R. (pbaez@ug.uchile.cl)
-% Date: 2018-09-08
+% Author: Pablo BÃ¡ez R. (pbaez@ug.uchile.cl)
+% Version: 1.0 (2018-09-08)
+% License: GPLv3
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 % get the contents of the clipboard and convert it to a java.lang.String object
 c = java.awt.Toolkit.getDefaultToolkit.getSystemClipboard;
