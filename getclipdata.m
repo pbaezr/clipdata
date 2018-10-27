0bfc158815805df20055f9c0d9f6d7a937f7a6f9
Pablo Báez R <pbaez@ug.uchile.cl>
2018-10-27 17:40:19 -0300

    Update getclipdata.m

--- getclipdata.m
+++ getclipdata.m
@@ -4,21 +4,10 @@ function data = getclipdata()
 %
 %   data = getclipdata
 
+% Licensed under the terms of the MIT License
+% Copyright (c) 2018 Pablo Baez R.
 % Author: Pablo Baez R. (pbaez@ug.uchile.cl)
 % Version: 1.0 (2018-09-08)
-% License: GPLv3
-%   This program is free software: you can redistribute it and/or modify
-%   it under the terms of the GNU General Public License as published by
-%   the Free Software Foundation, either version 3 of the License, or
-%   (at your option) any later version.
-%
-%   This program is distributed in the hope that it will be useful,
-%   but WITHOUT ANY WARRANTY; without even the implied warranty of
-%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-%   GNU General Public License for more details.
-%
-%   You should have received a copy of the GNU General Public License
-%   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 % --------------------------------------------------------------------------------------
 
 % get the contents of the clipboard and convert it to a java.lang.String object
@@ -40,4 +29,3 @@ numData = str2double(stringData);
 ind = isnan(numData);
 data = num2cell(numData);
 data(ind) = stringData(ind);
-
